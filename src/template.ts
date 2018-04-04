c2e0df7afbbfecb77f5f9246354b6bbc525e1cb2
Poul Kjeldager Sørensen <pks@s-innovations.net>
2018-04-04 22:03:30 +0200

    intial project

---
+++ src/template.ts
@@ -0,0 +1,68 @@
+//Not really sure where I got this from (copy/paste from project to project)
+//Code converted to typescript.
+
+import * as text from "text";
+import "./stringTemplateEngine";
+import ko = require("knockout");
+
+declare module "text" {
+    interface text {
+        load(name: string, req: any, onLoad: any, config: any): void;
+        write(pluginName: string, moduleName: string, write: any, config: any): void;
+    }
+    let a: text;
+    export = a;
+}
+declare module "text!*" {
+    let txt: string;
+    export = txt;
+}
+
+var loader = {
+    load: function (name: string, req: any, load: any, config: any) {
+        var onLoad = config.isBuild ? load : function (content: string) {
+
+            var safeName = inferTemplateName(name);
+            ko.templates[safeName] = content;
+            load(safeName);
+        };
+
+        text.load(name, req, onLoad, config);
+    },
+    write: function (pluginName: string, moduleName: string, write: any, config: any) {
+        text.write("text", moduleName, write, config);
+        var safeName = inferTemplateName(moduleName);
+
+        write.asModule(pluginName + "!" + moduleName,
+            "define(['text!" + moduleName + "', 'knockout', 'stringTemplateEngine'], function (content, ko) {" +
+            "ko.templates['" + safeName + "'] = content;" +
+            "return '"+safeName+"';" +
+            "});\n"
+        );
+    }
+};
+
+function inferTemplateName(name: string) {
+    var templateName: string;
+
+    var index = name.indexOf("!");
+    if (index !== -1) {
+        //use the template name that is specified
+        templateName = name.substring(index + 1, name.length);
+    } else {
+        //use the file name sans the path as the template name
+        var parts = name.split("/");
+        templateName = parts[parts.length - 1];
+
+        //remove the extension from the template name
+        var extensionIndex = templateName.lastIndexOf(".");
+        if (extensionIndex !== -1) {
+            templateName = templateName.substring(0, extensionIndex);
+        }
+    }
+    //  console.log(templateName);
+    return templateName;
+}
+
+
+export = loader;
---
+++ src/vendors.d.ts
@@ -0,0 +1,31 @@
+interface KnockoutBindingProvider {
+    preprocessNode?: (node: HTMLElement) => void;
+}
+interface KnockoutBindingHandlers {
+    koLayout: KnockoutBindingHandler;
+    react: KnockoutBindingHandler;
+}
+
+interface KnockoutTemplateSources {
+    stringTemplate: new (templateName: string) => KnockoutTemplateSourcesDomElement
+}
+
+interface KnockoutNativeTemplateEngine {
+    makeTemplateSource: (t: string | HTMLElement, d: Document) => KnockoutTemplateSourcesDomElement;
+}
+
+interface KnockoutStatic {
+    templates: { [name: string]: string }
+}
+declare module "text" {
+    interface text {
+        load(name: string, req: any, onLoad: any, config: any): void;
+        write(pluginName: string, moduleName: string, write: any, config: any): void;
+    }
+    let a: text;
+    export = a;
+}
+declare module "text!*" {
+    let txt: string;
+    export = txt;
+}
